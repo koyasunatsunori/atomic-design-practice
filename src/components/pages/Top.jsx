700cfb86128bdea8c4ec58c17363980fff203e75
koyasunatsunori <53142478+koyasunatsunori@users.noreply.github.com>
2021-12-28 21:25:50 +0900

    global state-2125

--- src/components/pages/Top.jsx
+++ src/components/pages/Top.jsx
@@ -1,6 +1,20 @@
+import { useContext } from "react";
+import { useHistory } from "react-router-dom";
 import styled from "styled-components";
+import { UserContext } from "../../providers/UserProvider";
 
 export const Top = () => {
+  const history = useHistory();
+  const { setUserInfo } = useContext(UserContext);
+
+  const onClickAdmin = () => {
+    setUserInfo({ isAdmin: true });
+    history.push("/users");
+  };
+  const onClickGeneral = () => {
+    setUserInfo({ isAdmin: false });
+    history.push("/users");
+  };
   return (
     <Scontainer>
       <h2>TOPページです</h2>
